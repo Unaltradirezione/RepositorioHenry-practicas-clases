b00c524098ef5ae92db3fabb3482bf6738384b14
unaltradirezione <unaltradirezione@hotmail.com>
2024-12-16 21:41:32 -0600

    cambios a operadores de comparacion

--- JavaScript/4 Operadores-de-comparacion/operadoresDeComparacion.js
+++ JavaScript/4 Operadores-de-comparacion/operadoresDeComparacion.js
@@ -16,7 +16,47 @@ console.log(4 == 7);
 
 
 // la forma de escribirlo para que la consola arroje resultado es escribiendo ""console. log ()""
-console. log (3 == '3');
+console. log (3 == 3);
 console. log (3 === 3);
 console. log (3 == '3');
 console. log (3 === '3');
+
+'Asociatividad '
+var a = 1;
+var b = 2;
+var c = (a = b);
+
+console. log (a); // tiene el valos de 2
+console. log (b); // tiene el valor de 2 
+console. log (c); // tiene el valor de 2   
+
+'ejemplo'
+    //la divicion tiene una asociatividad a la izquierda
+
+    console.log (16 / 2 / 4);
+
+
+
+//Operaciones de comparación
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+
+
+//Igualdad vs igualdad estricta 
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
+
+
+//Asignación y asociatividad
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log (c); 
+
+
