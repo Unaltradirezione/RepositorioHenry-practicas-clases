dab546cf0553aac1a8ae6cdd19d3e65707087728
unaltradirezione <unaltradirezione@hotmail.com>
2025-01-19 07:19:26 -0600

    cambios

--- JavaScript/12 Objeto Math/objeto Math.js
+++ JavaScript/12 Objeto Math/objeto Math.js
@@ -1,6 +1,37 @@
 
+                                                    Math.pow()
+//Este permite potenciar numeros, en donde el primer numero es la base y el segundo la potencia
+            Math.pow(2,3);
+                Ejemplo
+                console.log(Math.pow(2,3));
+                // =8
 
-Math,pow(2,3);
-console.log(Math.pow(2,3));
 
-Math.random();
+                                        Math.roun & Math.floor & Math.ceil
+
+//metodos para redondear un numero decimal, en JavaScript hay tres metodos 
+
+
+            Math.round()
+                //redondea el decimal al entero mas proximo
+                    Ejemplo
+                    console.log(Math.round(0.49))
+                    // = 0
+                    console.log(Math.round(0.50))
+                    // = 1
+
+            Math.floor()
+                //redondea el numero decimal al entero de menor valor
+                    Ejemplo
+                    console.log(Math.floor(5.93))
+
+
+            Math.ceil()
+                //redondea un numero decimalal siguiente entero
+                Ejemplo
+                console.log(Math.ceil(3.27))
+                // = 4
+
+
+
+
