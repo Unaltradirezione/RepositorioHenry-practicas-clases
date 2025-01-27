1fd882d5cc1a733b1de8cc44f48f626a240d7682
unaltradirezione <unaltradirezione@hotmail.com>
2025-01-27 08:33:20 -0600

    cambios a operadores logicos y se crea veracidad

---
+++ 6 Bucles/2 Veracidad/veracidad.js
@@ -0,0 +1,13 @@
+
+
+
+// Ejemplos con la función Boolean
+console.log(Boolean(1));          // true
+console.log(Boolean(0));          // false
+console.log(Boolean(-1));         // true
+console.log(Boolean(true));       // true
+console.log(Boolean(false));      // false
+console.log(Boolean('string'));   // true
+console.log(Boolean(null));       // false
+console.log(Boolean(undefined));  // false
+console.log(Boolean([]));         // true
