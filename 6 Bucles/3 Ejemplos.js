ff539215f218a39ce346591e79bdf03f3ffcda94
unaltradirezione <unaltradirezione@hotmail.com>
2025-01-27 08:19:58 -0600

    cambio a bucles

---
+++ 6 Bucles/3 Ejemplos.js
@@ -0,0 +1,25 @@
+
+                                                                    //todo    Operador and
+
+
+       //Todo  Primer Ejmeplo          
+
+    //Todo Evaluar la expresión lógica
+    let resultado = (5 < 2) && (5 > 3);
+
+    //Todo Mostrar el resultado en la consola
+    console.log(resultado); // false
+
+
+                                                                    // todo     Operador or
+
+
+
+    resultado = ( (5 < 2) || (5 >3)) //evalua la expresion logica
+     console.log  (resultado) // true
+
+
+                                                                    /// todo     operador not
+
+    Resultado = ! (5 < 2) //evalua la expresion logica
+    console.log (resultado)  // true                                                           
